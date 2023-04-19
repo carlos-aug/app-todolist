8c436510ea90662de4e664ff75992ef960a31ddb
Carlos Augusto <caarlos.san@gmail.com>
2023-04-19 19:26:02 -0300

    add component TodoList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import TodoList from "./components/TodoList";
+
 function App() {
   return (
     <div className="App">
-      <h1> TodoList </h1>
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,9 @@
+function TodoList(params) {
+  return (
+    <div>
+      <h1> TodoList </h1>
+    </div>
+  );
+}
+
+export default TodoList;
