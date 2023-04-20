a83d59c280bc25b7a63abd4fc48bf76ab26113c7
Carlos Augusto <caarlos.san@gmail.com>
2023-04-19 21:56:26 -0300

    add handleTask and handleSubmitTask

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,28 @@
+import React, { useState } from "react";
+
 function TodoList(params) {
+  const [task, setTask] = useState("");
+  const [listTasks, setListTasks] = useState([]);
+
+  function handleTask(event) {
+    setTask(event.target.value);
+  }
+
+  function handleSubmitTask() {
+    setListTasks([...listTasks, task]);
+    setTask("");
+  }
+
   return (
     <div>
       <h1> TodoList </h1>
+      <input type="text" value={task} onChange={handleTask} />
+      <button type="button" onClick={handleSubmitTask}>
+        Salvar
+      </button>
+      {listTasks.map((item, index) => (
+        <li>{item}</li>
+      ))}
     </div>
   );
 }
